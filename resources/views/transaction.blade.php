fc00d9291eee52d92e33a8ad8c00c14e8cc0f55f
islamsamy214 <islamsamy214@gmail.com>
2023-12-19 12:34:31 +0400

    Add PayPal integration and transaction view

---
+++ resources/views/transaction.blade.php
@@ -0,0 +1,4 @@
+<div>
+    {{-- smiple transaction link redirects to the paypal transactio route --}}
+    <a href="{{ route('processTransaction') }}">Paypal Transaction</a>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PayPalController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('create-transaction', [PayPalController::class, 'createTransaction'])->name('createTransaction');
+Route::get('process-transaction', [PayPalController::class, 'processTransaction'])->name('processTransaction');
+Route::get('success-transaction', [PayPalController::class, 'successTransaction'])->name('successTransaction');
+Route::get('cancel-transaction', [PayPalController::class, 'cancelTransaction'])->name('cancelTransaction');
